ad9e993498e7853c942a1f138be2b443d28086f7
Alexander Luizaga <a.luizaga10@gmail.com>
2021-06-14 13:17:08 -0300

    Se agregan validaciones al Json con express-validator

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express'); // incluimos el modulo express (modulos son bibliotecas) 
 const router = express.Router(); // Router, importante para poder diferenciar las peticiones al maximo
 
+const { body, validationResult } = require('express-validator'); // Se incluye modulo validator
+
 var app = express(); // Iniciamos express
 
 app.use(express.json()); // Recibir peticiones en formato Json
@@ -8,16 +10,28 @@ app.use(express.urlencoded({extended: false}));
 
 app.use(router); // Agregamos router a nuestra aplicacion de express para separa los verbos
 
-router.post('/', function(req, res){
-
-   // console.log(req.body);
-
-    const{nombre, apellido, dni} = req.body;
+router.post('/', 
+    // Validacion de Json
+    body("nombre").not().isEmpty(),
+    body("nombre").isString(),
+    body("apellido").not().isEmpty(),
+    body("apellido").isString(),
+    body("dni").isNumeric().isLength({max: 10}),
+    /* falta validar cantidad de parametos */
     
-    console.log(nombre);
-    console.log(apellido);   
+    function(req, res){
+        // Si hay un error en algunas de las validaciones se devuelve un objetoError y el codigo de
+        // estado 400
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        }
     
-    res.status(202).send("Todo ok");
+    // La validacion es correcta se devuelve cod. estado 201
+    console.log(req.body.nombre);
+    console.log(req.body.apellido);   
+    
+    res.status(201).send("Todo ok");
     
 });
 

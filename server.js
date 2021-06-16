4921f46aa2bd4674fa3004185d4678ed325ddb04
Alexander Luizaga <a.luizaga10@gmail.com>
2021-06-16 10:42:21 -0300

    Se actualiza comentarios del archivo server.js

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const express = require('express'); // incluimos el modulo express (modulos son bibliotecas) 
+const express = require('express'); // Se incluye el modulo express para la creacion del servidor
 const router = express.Router(); // Router, importante para poder diferenciar las peticiones al maximo
-const rp = require('request-promise'); //Se agrega el modulo de request-promise para realizar el POST
+const rp = require('request-promise'); //Se agrega el modulo de request-promise para realizar el POST y el GET
 
-// Se incluye modulo validator para realizar las validaciones del JSON
+// Se incluye modulo validator para realizar las validaciones de los atributos del JSON
 const { body, validationResult } = require('express-validator'); 
 
-var app = express(); // Iniciamos express
+var app = express(); // Se inicia express
 
 app.use(express.json()); // Recibir peticiones en formato Json
 app.use(express.urlencoded({extended: false}));
 
-app.use(router); // Agregamos router a nuestra aplicacion de express para separa los verbos
+app.use(router); // Agregamos router a nuestra aplicacion de express para separar los verbos
 
+//POST
 router.post('/', 
     // Validacion del Json (no envio ningun mje acerca del error, solo devuelvo codigo de respuesta 400)     
     body("nombre").not().isEmpty(),
@@ -31,7 +32,7 @@ router.post('/',
             return res.status(400).send();
         }
 
-    // Con los datos del Json ya validados se insertan en la BD realizando un POST a la URL dada:
+    // Con los datos del Json ya validados se insertan en la BD realizando un POST a la URL dada.
     // EL objeto "options" se utiliza para setear las opciones de la peticion POST
     // En el body se completa con los datos validados de la peticion anterior
     var options = {
@@ -64,13 +65,14 @@ router.post('/',
 // Se incluye el modulo path para poder usar el directorio raiz del proyecto
 const path = require('path'); 
 
+//GET
 router.get('/', function(req, res){    
-    // Se entrega como respuesta al GET un HTML    
+    // Se entrega como respuesta al GET un HTML (formulario)    
     // __dirname es una constante que contiene la ruta raiz del proyecto
     res.status(200).sendFile(path.join(__dirname+"/crearPersonas.html"));    
 });
 
-
+// La app eutiliza el puerto 3000 para escuchar las peticiones
 app.listen(3000);
 
 console.log("La aplicacion esta escuchando en http://localhost:3000");
